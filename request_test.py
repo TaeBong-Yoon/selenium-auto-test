c8471719f8c61df33ffef8d9379ab0e2ccfb1ef4
TaeHyun Yoon <62105316+TaeBong-Yoon@users.noreply.github.com>
2021-03-21 01:23:24 +0900

    Update request_test.py

--- request_test.py
+++ request_test.py
@@ -1,37 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
-
 import requests
-
 from bs4 import BeautifulSoup as bs
+import re
 
-from time import sleep
+req = requests.get('https://www.4nb.co.kr/v2/index.php')
+raw = req.text
 
-driver = webdriver.Chrome(r'C:\chromedriver.exe')
+text = re.sub('<!-.+?->','',raw,0,re.I|re.S)
 
-driver.implicitly_wait(3)
+html = bs(text, 'html.parser')
 
-driver.get('https://www.4nb.co.kr/v2/index.php')
-driver.maximize_window()
-
-wait = WebDriverWait(driver,10)
-
-html = driver.page_source
-
-soup = bs(html,'html.parser')
-
-
-#위 html 주소 = 4nb 홈페이지를 불러온 후
-#bs4 를 이용하여 해당 사이트의 모든 소스코드를 가져온다.
-
-#이후 아래에서는 가져온 소스코드에서 a태그의 href, area태그의 href를 불러와
-#http://help.4nb.co.kr에 합쳐준다.
-#합쳐진 url을 request한 뒤 response값을 받는다.
-#이 때, 200을 제외한 나머지는 모두 에러로 취급하여 콘솔에 표시한다.
-
+soup = html.find('body')
 
 # a태그
 print('a sector')
@@ -88,8 +66,3 @@ for area in soup.find_all('area', href=True):
             if response.status_code != 200:
                   print("error url: ",url)
                   print("error code:",response.status_code)
-
-
-sleep(3) 
-
-driver.quit()

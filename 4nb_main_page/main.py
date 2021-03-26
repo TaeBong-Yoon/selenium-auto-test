b90a0b849faafc6c1f8aebc71d62533d1f819012
TaeBong-Yoon <fkdlwldxogus@gmail.com>
2021-03-26 13:51:54 +0900

    modify main code

--- 4nb_main_page/main.py
+++ 4nb_main_page/main.py
@@ -8,17 +8,25 @@ from selenium.webdriver.common.alert import Alert
 
 from time import sleep
 
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+
 import carousel_controller as ct
 import footer_controller as fc
 import input_controller as ic
 import top_nav_controller as tc
 import mid_contents_controller as mc
 
+import href_tag_controller as hc
+
+test_url_page = 'https://www.4nb.co.kr/v2/index.php'
+
 driver = webdriver.Chrome(r'C:\chromedriver.exe')
 
 driver.implicitly_wait(3)
 
-driver.get('https://www.4nb.co.kr/v2/index.php')
+driver.get(test_url_page)
 driver.maximize_window()
 
 wait = WebDriverWait(driver,10)
@@ -31,16 +39,4 @@ mc.run(driver,wait,By,EC,ActionChains,sleep)
 
 driver.quit()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+hc.run(test_url_page,requests,bs,re)
